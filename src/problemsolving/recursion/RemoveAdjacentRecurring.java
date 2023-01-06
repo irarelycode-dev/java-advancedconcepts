fbab20e2ebc110c937cda7a52e10c0b56d2fbf35
vignesh <irarelycode@gmail.com>
2023-01-06 06:34:32 +0530

    remove adjacent recurring duplicate characters

---
+++ src/problemsolving/recursion/RemoveAdjacentRecurring.java
@@ -0,0 +1,21 @@
+package problemsolving.recursion;
+
+public class RemoveAdjacentRecurring {
+
+    public String removeAdjacent(String str){
+        if(str.length()==0 || str.length()==1) return str;
+        if(str.charAt(0)==str.charAt(1)){
+            while(str.charAt(0)==str.charAt(1)){
+                str=str.substring(1,str.length());
+            }
+            return removeAdjacent(str);
+        }else{
+            return str.charAt(0)+removeAdjacent(str.substring(1));
+        }
+    }
+
+    public static void main(String[] args) {
+        String word="geeeeeeksfffffoooooooorgeeeeeeeeeks";
+        System.out.println(new RemoveAdjacentRecurring().removeAdjacent(word));
+    }
+}
